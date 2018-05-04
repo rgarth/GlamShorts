ab9273f9f5d0ddcc653ed954893daea54901d248
Rob Garth <rgarth@linuxfoundation.org>
2018-05-04 23:10:28 +1000

    Adding some web pages to be able use the api

---
+++ web/js/shorten.js
@@ -0,0 +1,41 @@
+function isUrlValid(url) {
+    return /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+}
+
+function submitform() {
+  var shortenAPI = "https://your aws api endpoint.amazonaws.com/prod/shorten"
+  var redirectAPI = "https://your aws api endpoint.amazonaws.com/prod"
+  longURL = $('#url').val();
+  
+  if (! isUrlValid(longURL)) {
+    $( "#short" ).val("Error: Invalid URL");
+    return;
+  }
+  
+
+  $.ajax({
+    type: "POST",
+    url: shortenAPI,
+    data: JSON.stringify({"url": longURL}),
+    success: function(data, textStatus, XmlHttpRequest) {
+      if (XmlHttpRequest.status === 200) {
+        $( "#short" ).val(redirectAPI + "/" + data["short_url"]);
+        $( "#short" ).prop( "disabled", false );
+      } else {
+        $( "#short" ).val("Error: Response " + XmlHttpRequest.status);
+      }
+    },
+    error: function(XMLHttpRequest, textStatus, errorThrown) {
+      $( "#short" ).val("Error: Response " + textStatus);
+    },
+
+    dataType: "json",
+    contentType : "application/json"
+  });
+}
+
+function copyresult() {
+  $("#short").select();
+  document.execCommand("Copy");
+}
+
